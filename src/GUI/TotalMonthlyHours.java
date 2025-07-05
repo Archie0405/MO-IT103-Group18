c4631abf3ce6fb2dc82f077121635bb0d9adf264
USER <USER@Archie>
2025-07-05 20:59:51 +0800

    Added the year when computing the salary info of the employee.

--- src/GUI/TotalMonthlyHours.java
+++ src/GUI/TotalMonthlyHours.java
@@ -10,6 +10,8 @@ import java.io.IOException;
 import java.time.LocalDate;
 import java.time.LocalTime;
 import java.time.format.DateTimeFormatter;
+import java.time.format.TextStyle;
+import java.util.Locale;
 import javax.swing.JOptionPane;
 
 /**
@@ -18,7 +20,7 @@ import javax.swing.JOptionPane;
  */
 public class TotalMonthlyHours {
    //This is where we compute the total hours worked in a selected month with the use ofd the attendance records.
-    public static double computeTotalMonthlyHours(String employeeID, String selectedMonth) {
+    public static double computeTotalMonthlyHours(String employeeID, String selectedMonth, String selectedYear) {
         double totalHours = 0.0;
         try (BufferedReader br = new BufferedReader(new FileReader(ViewEmployee.ATTENDANCE_RECORD_CSV))) {
             br.readLine(); 
@@ -33,10 +35,14 @@ public class TotalMonthlyHours {
                     //We convert here the csv date string into a localDate Object
                     LocalDate recordDate = LocalDate.parse(date, DateTimeFormatter.ofPattern("MM-dd-yyyy"));
                     
+                    String recordMonth = recordDate.getMonth().getDisplayName(TextStyle.FULL, Locale.ENGLISH);
+                    String recordYear = String.valueOf(recordDate.getYear());
+                    
                     //Here it will check if the records is match to the selected month
-                    if (recordDate.getMonth().toString().equalsIgnoreCase(selectedMonth)) {
+                    if (recordMonth.equalsIgnoreCase(selectedMonth) && recordYear.equals(selectedYear)) {
                         LocalTime loginTime = LocalTime.parse(record[4]);
                         LocalTime logoutTime = LocalTime.parse(record[5]);
+
                         
                         //Now it calculates the hours worked base on the timestamp of login and logout in the csv file
                         double hoursWorked = logoutTime.getHour() - loginTime.getHour(); // Basic computation
